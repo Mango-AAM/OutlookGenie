329801b0beb5a01b09d7b5d7a01423d252a096d7
FuriousApe <abhinavtadi7@gmail.com>
2023-12-06 03:45:40 -0500

    Working Program. Assistants API still in Beta stage so might return wrong output. Try using GPT-4 and check if that increases the performance.

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,14 +1,12 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
+import { async } from 'regenerator-runtime';
 
 //import { get } from "core-js/core/dict"
 const axios = require('axios');
 
-/* global document, Office */
+const OpenAI = require('openai');
 
-const API_KEY = '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'
+const openai = new OpenAI({apiKey: '', dangerouslyAllowBrowser: true, version: 'v1'});
+
 const submitButton = document.getElementById("submit")
 const output = document.getElementById("output")
 const inputElement = document.querySelector('input')
@@ -18,35 +16,63 @@ const conversationHistory = [{role: "system", content: "You are a helpful assist
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
-    Office.context.mailbox.getCallbackTokenAsync({isRest: true}, function(result){
+    Office.context.mailbox.getCallbackTokenAsync({isRest: true}, async function(result){
       if (result.status === "succeeded") {
         const accessToken = result.value;
-        submitButton.addEventListener('click',getMessage);
+
+        const assistant = await openai.beta.assistants.create({
+          // See if you can come up with a better instruction
+          instructions: "You are an Outlook QA assistant, who answers questions by making calls to Outlook to retrieve user emails and using that information to successfully answer the users questions. When the user types lets start fresh you clear all messages in the thread.",
+          name: "Outlook QA Assistant",
+          model: "gpt-3.5-turbo-1106",
+          tools: [{
+            "type": "function",
+            "function": {
+              "name": "getAllMessages",
+              "description": "Get the user's emails from Outlook based on a search keyword and/or sender email address and/or number of emails to retrieve",
+              "parameters": {
+                  "type": "object",
+                  "properties": {
+                      "accessToken": {
+                          "type": "string",
+                          "description": "The access token for the user's Outlook account. Required for authentication.",
+                      },
+                      "searchKeyword": {
+                          "type": "string",
+                          "description": "The keyword to search for in the user's emails and return all emails that match the keyword.",
+                      },
+                      "from": {
+                          "type": "string",
+                          "description": "The sender email address to search for in the user's emails and return all emails that match the sender email address.",
+                          "unit": {
+                            "type": "string",
+                            "pattern": "^\\S+@\\S+\\.\\S+$"
+                          }
+                      },
+                      "top": {
+                          "type": "integer",
+                          "description": "The number of emails to retrieve from the user's emails."
+                      }
+                  },
+                  "required": ["accessToken"] 
+              }
+            }
+          }]
+        });
+
+        const thread = await openai.beta.threads.create();
+
+        submitButton.addEventListener('click', () => getMessage(assistant.id, accessToken, thread.id));
+
+
+
         buttonElement.addEventListener('click', clearInput);
-        console.log(accessToken);
-
-        const restHost = Office.context.mailbox.restUrl;
-        const searchKeyword = 'kill';
-        const getMessageUrl = Office.context.mailbox.restUrl + `/v2.0/me/messages?$search=${encodeURIComponent(searchKeyword)}`;
-        getAllMessages(accessToken, getMessageUrl, searchKeyword);
-        console.log("Hahahaha");
-
       } else {
         console.error(result.error);
       }
     });
   }
 });
-
-function getItemRestId() {
-  if (Office.context.mailbox.diagnostics.hostName === 'OutlookIOS') {
-    // itemId is already REST-formatted.
-    return Office.context.mailbox.item.itemId;
-  } else {
-    // Convert to an item ID for API v2.0.
-    return Office.context.mailbox.convertToRestId(Office.context.mailbox.item.itemId, Office.MailboxEnums.RestVersion.v2_0);
-  }
-}
 
 function changeInput(value){
   inputElement.value = value
@@ -56,80 +82,201 @@ function clearInput(){
   inputElement.value = ''
 }
 
+function getTextFromHtml(htmlString) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlString, "text/html");
+  const textContent = doc.body.textContent;
+  return textContent;
 
-function getAllMessages(accessToken, getMessageUrl, searchKeyword) {
-  console.log("Hahahaha");
-  //const itemId = getItemRestId();
-  //console.log(itemId);
+  /* Write Code here
+  textContent is a string with the body of each email. This is where you need to mask the Private information and return the masked string.
+  Either make another function and call it here, or edit this function such that it directly returns the masked string.*/
 
-  axios.get(getMessageUrl, {
-    headers: {
-        'Authorization': 'Bearer ' + accessToken
+}
+
+
+function parseCCRecipients(ccRecipients) {
+  let ccRecipientsArray = [];
+  if (ccRecipients && ccRecipients.length > 0) {
+    ccRecipients.forEach(function (ccRecipient) {
+      let cc = {
+        "name": ccRecipient.EmailAddress.Name,
+        "emailAddress": ccRecipient.EmailAddress.Address
+      };
+      ccRecipientsArray.push(cc);
+    });
+  }
+  return ccRecipientsArray;
+}
+
+function getAllMessages(params) {
+  let { accessToken, searchKeyword = "", from = "None", top = 500 } = params;
+  return new Promise(async (resolve, reject) => {
+    let messages = [];
+    let url;
+
+    if (searchKeyword === "" && from !== "None") {
+      url = Office.context.mailbox.restUrl + `/v2.0/me/messages?$filter=from/emailAddress/address eq '${from}'&$top=${top}`;
+    } else if (searchKeyword !== "" && from === "None") {
+      url = Office.context.mailbox.restUrl + `/v2.0/me/messages?$search=${encodeURIComponent(searchKeyword)}&$top=${top}`;
+    } else if (searchKeyword !== "" && from !== "None") {
+      url = Office.context.mailbox.restUrl + `/v2.0/me/messages?$search=${encodeURIComponent(searchKeyword)}&$filter=from/emailAddress/address eq '${from}'&$top=${top}`;
     }
-  })
-  .then(async function (response) {
-    console.log(response.data);
-    console.log(response.data.value.length);
-    if (response.data.value && response.data.value.length > 0) {
-      // Loop through each message in the result
-      response.data.value.forEach(async function(message) {
-        const subject = await message.Subject;
-        console.log(subject);
-        const body = await message.BodyPreview;
-        console.log(`Body: ${body}`);
-      });
+
+    async function fetchMessages(url) {
+      try {
+        const response = await axios.get(url, {
+          headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'Prefer': 'outlook.body-content-type="text"',
+            "InferenceClassification": "Focused"
+          }
+        });
+
+        //console.log(response.data);
+        //console.log(response.data.value.length);
+
+        if (response.data.value && response.data.value.length > 0) {
+          response.data.value.forEach(function (message) {
+            let messagejson = {
+              "subject": message.Subject,
+              "body": getTextFromHtml(message.Body.Content).replace(/\s+/g, ' ').trim(),
+              "sender": message.Sender.EmailAddress.Address,
+              "receivedDateTime": message.ReceivedDateTime,
+              "id": message.Id,
+              "importance": message.Importance,
+              "isRead": message.IsRead,
+              "isReadReceiptRequested": message.IsReadReceiptRequested,
+              "CCRecipients": parseCCRecipients(message.CcRecipients),
+              "BCCRecipients": parseCCRecipients(message.BccRecipients),
+            }
+            messages.push(messagejson);
+          });
+        }
+
+        if (response.data["@odata.nextLink"]) {
+          await fetchMessages(response.data["@odata.nextLink"]);
+        } else {
+          resolve(messages); // Resolve the promise when all messages are fetched
+        }
+      } catch (error) {
+        console.error('Error from Axios:', error);
+        // Stop the call and return the specified string on error
+        resolve(messages);
+      }
     }
-    if (response.data["@odata.nextLink"]) {
-      await getAllMessages(accessToken, response.data["@odata.nextLink"]);
-  }})
-  .catch(function (error) {
-      console.error('Error from Axios:', error);
+
+    fetchMessages(url);
   });
 }
 
 
-export async function getMessage(){
+
+export async function getMessage(assistant_id, accessToken, thread_id) {
   console.log("Clicked");
-  conversationHistory.push({role: "user", content: inputElement.value})
-  const options = {
-      method: 'POST',
-      headers: {
-          'Authorization': `Bearer ${API_KEY}`,
-          'Content-Type' : 'application/json'
-      },
-      body: JSON.stringify({
-          model: "gpt-3.5-turbo",
-          messages: conversationHistory,
-          temperature: 0.7,
-          max_tokens: 100
-        })
-          
-  }
-  try{
-      const response = await fetch('https://api.openai.com/v1/chat/completions',options)
-      const data = await response.json()
-      console.log(options)
-      console.log("Hahahaha")
-      console.log(data)
-      output.textContent = data.choices[0].message.content
-      conversationHistory.push(data.choices[0].message)
-      const pElement = document.createElement('p')
-      pElement.textContent = inputElement.value
-      pElement.addEventListener('click',()=>changeInput(pElement.textContent))
-      history.append(pElement)
 
-  }catch(error){
-      console.error(error);
+  const message = await openai.beta.threads.messages.create(
+    thread_id,
+    {
+      role: "user",
+      content: inputElement.value
+    }
+  );
+
+  const run = await openai.beta.threads.runs.create(
+    thread_id,
+    { 
+      assistant_id: assistant_id,
+      //instructions: "Please address the user as Jane Doe. The user has a premium account."
+    }
+  );
+
+  let runstatus = await openai.beta.threads.runs.retrieve(
+    thread_id,
+    run.id
+  );
+
+
+  while (runstatus.status !== "completed") {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    runstatus = await openai.beta.threads.runs.retrieve(
+      thread_id,
+      run.id
+    );
+
+    if (runstatus.status === "requires_action") {
+      const toolCalls =
+        runstatus.required_action.submit_tool_outputs.tool_calls;
+      const toolOutputs = [];
+
+      for (const toolCall of toolCalls) {
+        const functionName = toolCall.function.name;
+
+        console.log(
+          `This question requires us to call a function: ${functionName}`
+        );
+
+        const args = JSON.parse(toolCall.function.arguments);
+        //console.log(args);
+        args.accessToken = accessToken;
+
+        const argsArray = Object.keys(args).map((key) => args[key]);
+
+        // Dynamically call the function with arguments
+        const output5 = await window[functionName].apply(null, [args]);
+
+        //console.log(`Output: ${output5}`);
+        //console.log(output5);
+
+        toolOutputs.push({
+          tool_call_id: toolCall.id,
+          output: `${output5}`,
+        });
+      }
+      // Submit tool outputs
+      await openai.beta.threads.runs.submitToolOutputs(
+        thread_id,
+        run.id,
+        { tool_outputs: toolOutputs }
+      );
+      continue; // Continue polling for the final response
+    }
+
+    // Check for failed, cancelled, or expired status
+    if (["failed", "cancelled", "expired"].includes(runstatus.status)) {
+      console.log(
+        `Run status is '${runstatus.status}'. Unable to complete the request.`
+      );
+      break; // Exit the loop if the status indicates a failure or cancellation
+    }
   }
+
+  // Get the last assistant message from the messages array
+  const messages = await openai.beta.threads.messages.list(thread_id);
+
+  // Find the last message for the current run
+  const lastMessageForRun = messages.data
+    .filter(
+      (message) =>
+        message.run_id === run.id && message.role === "assistant"
+    )
+    .pop();
+
+  // If an assistant message is found, console.log() it
+  if (lastMessageForRun) {
+    console.log(`${lastMessageForRun.content[0].text.value} \n`);
+  } else if (
+    !["failed", "cancelled", "expired"].includes(runstatus.status)
+  ) {
+    console.log("No response received from the assistant.");
+  }
+
+  output.textContent = lastMessageForRun.content[0].text.value;
+  const pElement = document.createElement('p')
+  pElement.textContent = inputElement.value
+  pElement.addEventListener('click',()=>changeInput(pElement.textContent))
+  history.append(pElement)
+
 }
 
-export async function run() {
-  // Get a reference to the current message
-  const item = Office.context.mailbox.item;
-
-  // Write message property value to the task pane
-  //document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
-  /**
-   * Insert your Outlook code here
-   */
-}
+window.getAllMessages = getAllMessages;
